3639da1ee026313a1d3e412690c422d15a2b02c0
AdrienHA <a_ha@hetic.eu>
2024-06-24 22:56:34 +0200

    add page search

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,23 +1,54 @@
-import "./Header.css"
+'use client';
+
+import React, { useState } from "react";
+import { useRouter } from 'next/navigation';
+import "./Header.css";
 import { logout } from "../logout/actions";
 import { Search } from "@mui/icons-material";
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Header() {
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSearchChange = (event) => {
+    const searchQuery = event.target.value;
+    setQuery(searchQuery);
+  };
+
+  const handleSearch = () => {
+    if (query.length > 2) {
+      setLoading(true);
+      router.push(`/search?keyword=${query}`);
+    }
+  };
+
   return (
     <div className="header">
-        <div className="menu">
-            <h2>HOME</h2>
-            <h2>LISTE ANIME</h2>
-            <h2>NOUVELLE SAISON</h2>
-            <h2>FILMS</h2>
-            <h2>POPULAIRE</h2>
-        </div>
-        <div className="search-container">
-            <Search />
-        </div>
-        <form action={logout}>
-          <button type="submit" className="logout-button">Logout</button>
-        </form>
+      <div className="menu">
+        <h2>HOME</h2>
+        <h2>LISTE ANIME</h2>
+        <h2>NOUVELLE SAISON</h2>
+        <h2>FILMS</h2>
+        <h2>POPULAIRE</h2>
+      </div>
+      <div className="search-container">
+        <input
+          type="text"
+          value={query}
+          onChange={handleSearchChange}
+          placeholder="Rechercher un anime..."
+          className="search-input"
+        />
+        <button className="search-button" onClick={handleSearch}>
+          <Search />
+        </button>
+        {loading && <CircularProgress size={20} className="loader" />}
+      </div>
+      <form action={logout}>
+        <button type="submit" className="logout-button">Logout</button>
+      </form>
     </div>
   );
 }
---
+++ app/search/page.js
@@ -0,0 +1,63 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
+import "./search.css";
+import Header from '../components/Header';
+
+export default function SearchPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get('keyword');
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (keyword) {
+      const fetchResults = async () => {
+        setLoading(true);
+        try {
+          const response = await axios.get(`https://api.jikan.moe/v4/anime?q=${keyword}&sfw`);
+          const sortedResults = response.data.data.sort((a, b) => a.title.localeCompare(b.title));
+          setResults(sortedResults);
+        } catch (error) {
+          console.error("Erreur lors de la récupération des données : ", error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchResults();
+    }
+  }, [keyword]);
+
+  return (
+    <div className="anime-app">
+      <div className="container">
+        <Header />
+        <div className="content">
+          <div className="media-section">
+            <div className="media-header">
+              <div className="media-title">
+                <h2>Résultats de recherche pour : {keyword}</h2>
+              </div>
+            </div>
+            <div className="anime-grid upcoming">
+              {loading && <CircularProgress size={50} />}
+              {results.map((anime) => (
+                <div key={anime.mal_id} className="anime-card">
+                  <img className="image" src={anime.images.jpg.image_url} alt={anime.title} />
+                  <div className='anime'>
+                    <h3>{anime.title}</h3>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
